9fc2a8def022b61dd658456190148d72fb8bda74
haleywitherell <hwitherell22@gmail.com>
2022-01-12 14:48:41 -0500

    updates

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -12,75 +12,71 @@ const Portfolio = () => {
         <h2>Work & Projects</h2>
         <p>Visit my GitHub account to view all past projects!</p>
 
-        <p2>Meal and Work Out Planner</p2>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://haleywitherell.github.io/Health-Wellness/" target="_blank">View Site</a>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/Health-Wellness" target="_blank">View on GitHub</a>
-
-        <div className="container d-flex justify-content-center">
-            <div className="comlumn">
-                <div id="img1">
-                    <img className="img-fluid rounded" src={planner} alt="Example of a weekly meal and workout planner"/>  
-                </div>
+    
+        <div className="col-12 col-lg-4">
+            <div className="card text-center h-100">
+                <img src={planner} className="card-img-top" alt="Meal and Workout Planner"/>
+                    <div className="card-body">
+                        <h3 className="card-title fw-bold">Meal and Workout Planner</h3>
+                        <p className="card-text">Need a unique password created on the fly? Use the random password generator app which creates random passwords using capital letters, lowercase letters, numbers and or symbols.</p>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://haleywitherell.github.io/Health-Wellness/" className="btn btn-outline-dark m-2" target="_blank">View Site</a>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/Health-Wellness" className="btn btn-outline-dark m-2" target="_blank">View on GitHub</a>
+                    </div>
             </div>
         </div>
 
-    
-        <p2>Password Generator</p2>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://haleywitherell.github.io/Password-Generator/" target="_blank">View Site</a>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/Password-Generator" target="_blank">View on GitHub</a>
+
+        <div className="col-12 col-lg-4">
+            <div className="card text-center h-100">
+                <img src={password} className="card-img-top" alt="Password Generator"/>
+                    <div className="card-body">
+                        <h3 className="card-title fw-bold">Password Generator</h3>
+                        <p className="card-text">Need a unique password created on the fly? Use the random password generator app which creates random passwords using capital letters, lowercase letters, numbers and or symbols.</p>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://haleywitherell.github.io/Password-Generator/" className="btn btn-outline-dark m-2" target="_blank">View Site</a>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/Password-Generator" className="btn btn-outline-dark m-2" target="_blank">View on GitHub</a>
+                    </div>
+            </div>
+        </div>
+
+        <div className="col-12 col-lg-4">
+            <div className="card text-center h-100">
+                <img src={travel} className="card-img-top" alt="Travel Blog"/>
+                    <div className="card-body">
+                        <h3 className="card-title fw-bold">Travel Blog</h3>
+                        <p className="card-text">Browse and write your own travel reviews of different cities, trips you've taken, etc. </p>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://intense-scrubland-56931.herokuapp.com/login" className="btn btn-outline-dark m-2" target="_blank">View Site</a>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/bckasper/Travel-blog" className="btn btn-outline-dark m-2" target="_blank">View on GitHub</a>
+                    </div>
+            </div>
+        </div>
+
+
+        <div className="col-12 col-lg-4">
+            <div className="card text-center h-100">
+                <img src={books} className="card-img-top" alt="Book Search App"/>
+                    <div className="card-body">
+                        <h3 className="card-title fw-bold">Book Search App</h3>
+                        <p className="card-text">Search for books using this app and build your own library by saving your favorite reads! </p>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://pure-wildwood-80529.herokuapp.com/" className="btn btn-outline-dark m-2" target="_blank">View Site</a>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/searching-books" className="btn btn-outline-dark m-2" target="_blank">View on GitHub</a>
+                    </div>
+            </div>
+        </div>
+
 
         
-
-        <div className="container d-flex justify-content-center">
-            <div className="comlumn">
-                <div id="img1">
-                    <img className="img-fluid rounded" src={password} alt="Example of a password generator app"/>  
-                </div>
+        <div className="col-12 col-lg-4">
+            <div className="card text-center h-100">
+                <img src={workout} className="card-img-top" alt="Fitness Tracker"/>
+                    <div className="card-body">
+                        <h3 className="card-title fw-bold">Fitness Tracker</h3>
+                        <p className="card-text">Put together and track your daily workouts using this app. Reference olds workouts using the last workout faeture</p>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://workout-tracker-app-12345.herokuapp.com/" className="btn btn-outline-dark m-2" target="_blank">View Site</a>
+                        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/workout-tracker" className="btn btn-outline-dark m-2" target="_blank">View on GitHub</a>
+                    </div>
             </div>
         </div>
 
-    
-        <p2>Travel Reviews Blog</p2>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://intense-scrubland-56931.herokuapp.com/login" target="_blank">View Site</a>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/bckasper/Travel-blog" target="_blank">View on GitHub</a>
-
-
-
-        <div className="container d-flex justify-content-center">
-            <div className="comlumn">
-                <div id="img1">
-                    <img className="img-fluid rounded" src={travel} alt="Example of a travel blog"/>  
-                </div>
-            </div>
-        </div>
-
-        <p2>Workout Tracker</p2>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://workout-tracker-app-12345.herokuapp.com/" target="_blank">View Site</a>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/workout-tracker" target="_blank">View on GitHub</a>
-
-
-
-        <div className="container d-flex justify-content-center">
-            <div className="comlumn">
-                <div id="img1">
-                    <img className="img-fluid rounded" src={workout} alt="Example of a workout tracker"/>  
-                </div>
-            </div>
-        </div>
-
-        <p2>Book Search App</p2>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://pure-wildwood-80529.herokuapp.com/" target="_blank">View Site</a>
-        <a className="container d-flex justify-content-center text-dark nav-link" href="https://github.com/haleywitherell/searching-books" target="_blank">View on GitHub</a>
-
-
-
-        <div className="container d-flex justify-content-center">
-            <div className="comlumn">
-                <div id="img1">
-                    <img className="img-fluid rounded" src={books} alt="Example of a workout tracker"/>  
-                </div>
-            </div>
-        </div>
     </main>
         
     <hr class="solid container"></hr>
