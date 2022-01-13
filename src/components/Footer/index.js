8205083acff03eff95e33c147d4a0f55aee7bd3f
haleywitherell <hwitherell22@gmail.com>
2022-01-13 15:24:44 -0500

    updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div>
             <footer className="jumbotron gradient">
-                <p3>Haley Witherell est. 2021 | </p3>
+                <p4>Haley Witherell est. 2021 | </p4>
                 <a className="footer-links" href="https://www.linkedin.com/in/haley-witherell-6a177315a/" target="_blank"> LinkedIn | </a>
                 <a className="footer-links" href="https://github.com/haleywitherell" target="_blank"> GitHub</a>
 
