dbe2fb13fe9532499205ee16ccec9bfb80d7df97
haleywitherell <hwitherell22@gmail.com>
2022-01-13 11:15:25 -0500

    renamed

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,7 +5,7 @@ const About = () => {
         <div>
             <section id="about" class="container">
         <h2>About Me</h2>
-            <p1>Hey i'm Haley! I am a Cincinnati based account manager for a graphic design, print and prepress agency.</p1>
+            <p1>Hey i'm Haley! I am currently a Cincinnati based account coordinator for a graphic design, print and prepress agency.</p1>
                 <br></br>
                 <br></br>
             <p1>I have experience in web devlopment, marketing, advertising, public relations, communications and design.</p1>
