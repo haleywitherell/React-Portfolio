0c9ead6afbbf6bafeb585719300beb7b0d1b4ae8
haleywitherell <hwitherell22@gmail.com>
2022-01-25 16:29:48 -0500

    icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { SiLinkedin, SiGithub} from 'react-icons/si'
+
 
 const Footer = () => {
     return (
@@ -6,8 +8,8 @@ const Footer = () => {
             <footer className="jumbotron gradient nav justify-content-center navbar navbar-expand-sm">
                 <p4>Haley Witherell est. 2021 |</p4>
                 <div className="navbar-nav">
-                    <a className="footer-links nav-link text-white" href="https://www.linkedin.com/in/haley-witherell-6a177315a/" target="_blank">LinkedIn |</a>
-                    <a className="footer-links nav-link text-white" href="https://github.com/haleywitherell" target="_blank">GitHub</a>
+                    <a href="https://www.linkedin.com/in/haley-witherell-6a177315a/" target="_blank" className='footer-links nav-link text-white icons'><SiLinkedin/></a>
+                    <a href="https://github.com/haleywitherell" target="_blank" className='footer-links nav-link text-white icons'><SiGithub/></a>
                 </div>
             </footer>
         </div>
