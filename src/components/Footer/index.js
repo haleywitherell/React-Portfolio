d8c92757a2f9175de36b43b0c810e89d31af47aa
haleywitherell <hwitherell22@gmail.com>
2022-01-25 20:36:36 -0500

    icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,12 +7,12 @@ const Footer = () => {
         <div>
             <footer className="jumbotron gradient nav justify-content-center navbar navbar-expand-sm">
                 <p4>Haley Witherell est. 2021 |</p4>
-                <div className="navbar-nav">
-                    <a href="https://www.linkedin.com/in/haley-witherell-6a177315a/" target="_blank" className='footer-links nav-link text-white icons'><SiLinkedin/></a>
-                    <a href="https://github.com/haleywitherell" target="_blank" className='footer-links nav-link text-white icons'><SiGithub/></a>
-                </div>
+                    <a href="https://www.linkedin.com/in/haley-witherell-6a177315a/" target="_blank" className='footer-links nav-link text-white navbar-text icons'><SiLinkedin/></a>
+                    <a href="https://github.com/haleywitherell" target="_blank" className='footer-links nav-link text-white navbar-text icons'><SiGithub/></a>
             </footer>
         </div>
+
+        
     )
 }
 
